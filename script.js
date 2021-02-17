88336ffbc3419a6917448a4183a790c2a131e01c
Leticia Dias <contatoleticiadia@gmail.com>
2021-02-17 16:21:43 -0300

    file

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ function changeScene(decision) {
 
         case 0:
             if(decision == 1){
-                curScene = 2
+                curScene = 2;
                 message = "You arrived at a beautiful little house in the forest";
             }else{
-                curScene = 3
-                message = "You are positioning a part overlooking a peaceful river"
+                curScene = 3;
+                message = "You are positioning a part overlooking a peaceful river";
             }
 
         break;
@@ -23,16 +23,24 @@ function changeScene(decision) {
 
         if(decision == 1){
             curScene = 0;
-            message = "Welcome back! Your journey begins now."
-        }
+            message = "Welcome back! Your journey begins now.";
+        }else {
+            decision == 2
+            message = "Essa ação não pode ser executada. Click novamente no primeiro botão para retornar ao início."
+        } 
+
+        break;
 
         case 3: 
 
         if(decision == 2){
             curScene = 0;
-            message = "Welcome back! Your journey begins now."
+            message = "Welcome back! Your journey begins now.";
+        }else {
+            decision == 1
+            message = "Essa ação não pode ser executada. Click novamente no segundo botão para retornar ao início."
         }
-
+         
     }
 
     document.getElementById('sceneimg').src = "assets/scene" + curScene + ".jpg";
